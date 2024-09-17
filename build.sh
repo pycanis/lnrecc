1b96830e1f74b09cacfeabc3c794a804f0c1f87f
pycanis <pycan@proton.me>
2024-09-17 15:02:23 +0200

    Release script and signatures

--- build.sh
+++ build.sh
@@ -1,24 +1,34 @@
 #!/bin/bash
 
+# clear previous release
+
+rm lnrecc*
+
+# version input
+
 version=$1
 
-if [ -z "$version" ]; then
-  echo "Error: No version argument provided."
+if [ -z $version ]; then
+  echo Error: No version argument provided.
   exit 1
 fi
 
 # ARM64 darwin
 cargo build --release
 
-APP_NAME="lnrecc"
-ARCHIVE_NAME="releases/${APP_NAME}-darwin-arm64-${version}.tar.gz"
+APP_NAME=lnrecc
+HASHES_NAME=$APP_NAME-$version.sha256.txt
+
+ARCHIVE_NAME=$APP_NAME-darwin-arm64-$version.tar.gz
 
 tar -czvf $ARCHIVE_NAME -C target/release $APP_NAME
 
+shasum -a 256 $ARCHIVE_NAME >> $HASHES_NAME
+
 # AMD64 linux
 
-BUILD_IMAGE_NAME="lnrecc-build"
-ARCHIVE_NAME="releases/${APP_NAME}-linux-amd64-${version}.tar.gz"
+BUILD_IMAGE_NAME=lnrecc-build
+ARCHIVE_NAME=$APP_NAME-linux-amd64-${version}.tar.gz
 
 docker build -f Dockerfile-build -t ${BUILD_IMAGE_NAME} --platform linux/amd64 .
 
@@ -27,6 +37,15 @@ container_id=$(docker run -d ${BUILD_IMAGE_NAME})
 docker cp $container_id:/usr/src/app/target/release/$APP_NAME $APP_NAME
 
 tar -czvf $ARCHIVE_NAME $APP_NAME
+
+shasum -a 256 $ARCHIVE_NAME >> $HASHES_NAME
+
+# sign hashes
+
+echo GPG passphrase:
+read -s passphrase
+
+gpg --batch --pinentry-mode loopback --detach-sign --passphrase $passphrase $HASHES_NAME
 
 # cleanup
 
